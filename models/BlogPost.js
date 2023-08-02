54d57b417e17b9a62a94b92b5e1d1e5de60c6a3d
phillipkujawa <36670901+phillipkujawa@users.noreply.github.com>
2023-08-02 06:18:24 -0400

    updated blogpost.js model

--- models/BlogPost.js
+++ models/BlogPost.js
@@ -4,3 +4,19 @@ const sequelize = require('../config/connection');
 class BlogPost extends Model {
     
   }
+  BlogPost.init(
+    {
+      blogtitle: {
+        type: DataTypes.STRING,
+      },
+      blogcontent: {
+        type: DataTypes.STRING,
+      },
+    },
+    {
+      sequelize,
+      underscored: true,
+    }
+  );
+  
+  module.exports = BlogPost;

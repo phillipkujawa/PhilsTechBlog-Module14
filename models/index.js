cf364932e2330f918ae5d4db1cd5af9392951b2a
phillipkujawa <36670901+phillipkujawa@users.noreply.github.com>
2023-08-02 06:25:45 -0400

    updated index.js

--- models/index.js
+++ models/index.js
@@ -2,8 +2,26 @@ const User = require("./User");
 const Post = reqiuire("./BlogPost");
 const Comment = require("./Comment");
 
+// create associations
+Post.belongsTo(User, {
+    foreignKey: "user_id",
+    onDelete: "CASCADE"
+});
+
+Post.hasMany(Comment, {
+    foreignKey: "post_id",
+    onDelete: "CASCADE"
+});
+
+Comment.belongsTo(User, {
+    foreignKey: "user_id",
+    onDelete: "CASCADE"
+});
+
+
 module.exports = { 
     User,
     Post,
     Comment
 }
+

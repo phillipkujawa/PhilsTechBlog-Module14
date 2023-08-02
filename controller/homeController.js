a2f7d0957a338a491394f760bebd097176d52aa8
phillipkujawa <36670901+phillipkujawa@users.noreply.github.com>
2023-08-02 06:56:42 -0400

    added homecontroller / get route

--- controller/homeController.js
+++ controller/homeController.js
@@ -1,5 +1,25 @@
 const router = require('express').Router();
-const { Post } = require('../models');
-const withAuth = require('../utils/auth');
+const { Post, Comment, User } = require('../models');
+
+router.get('/', async (req, res) => {
+    try {
+        const postData = await Post.findAll({
+            include: [
+                {
+                    model: User,
+                    attributes: ['username'],
+                },
+            ],
+        });
+    
+        const posts = postData.map((post) => post.get({ plain: true }));
+
+        res.render('homepage', {
+            posts
+        })
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 
 module.exports = router;
